4df9bbf4512a1183ebf644081d001fda3d1259af
MahbodHastam <mahbod4782@gmail.com>
2024-08-01 18:31:21 +0330

    fix urls

--- src/components/Home/AboutSection.vue
+++ src/components/Home/AboutSection.vue
@@ -6,6 +6,25 @@ import data from '@/data.json'
 
 gsap.registerPlugin(ScrollTrigger)
 
+const waysToConnectMe = [
+  {
+    key: 'E-Mail',
+    label: data.email,
+    url: `mailto:${data.email}`,
+  },
+  {
+    key: 'Github',
+    label: `@${data.github}`,
+    url: `https://github.com/${data.github}`,
+  },
+  { key: 'X', label: `@${data.x}`, url: `https://x.com/${data.x}` },
+  {
+    key: 'Telegram',
+    label: `@${data.telegram_username}`,
+    url: `https://t.me/${data.telegram_username}`,
+  },
+]
+
 onMounted(() => {
   gsap.to('#about', {
     y: 0,
@@ -20,17 +39,6 @@ onMounted(() => {
     },
   })
 })
-
-const waysToConnectMe = [
-  {
-    key: 'E-Mail',
-    label: 'mahbod4782@gmail.com',
-    url: 'mailto:mahbod4782@gmail.com',
-  },
-  { key: 'Github', label: '@MahbodHastam', url: 'https://github.com/MahbodHastam' },
-  { key: 'X', label: '@MahbodHastam', url: 'https://x.com/MahbodHastam' },
-  { key: 'Telegram', label: '@Bod_Bod', url: 'https://t.me/Bod_Bod' },
-]
 </script>
 
 <template>
@@ -44,19 +52,21 @@ const waysToConnectMe = [
       cumque magni esse rerum, blanditiis quod at temporibus nam reprehenderit
       ratione quia deserunt asperiores quis impedit. Quod, omnis fugit.
     </p>
-    <h2 class="mb-4 mt-12 text-xl font-medium">Ways to Connect me</h2>
-    <div class="mx-auto">
-      <ul class="list-disc space-y-2 marker:text-gray-400 sm:ml-5">
-        <li v-for="i in waysToConnectMe">
-          <span class="block sm:inline-flex sm:w-20">{{ i.key }}:</span>
-          <a
-            :href="i.url"
-            target="_blank"
-            class="ml-4 underline duration-100 hover:opacity-70"
-            >{{ i.label }}</a
-          >
-        </li>
-      </ul>
+    <div id="ways">
+      <h2 class="mb-4 mt-12 text-xl font-medium">Ways to Connect me</h2>
+      <div class="mx-auto">
+        <ul class="list-disc space-y-2 marker:text-gray-400 sm:ml-5">
+          <li v-for="i in waysToConnectMe">
+            <span class="block sm:inline-flex sm:w-20">{{ i.key }}:</span>
+            <a
+              :href="i.url"
+              target="_blank"
+              class="ml-4 underline duration-100 hover:opacity-70"
+              >{{ i.label }}</a
+            >
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
